fa83157c523d0f3c0a7d9e418212854044f47526
Vitalii Koretskyi <tolik.developer@gmail.com>
2021-09-15 23:27:31 +0200

    feat!: add equatable as dependency

--- lib/src/node.dart
+++ lib/src/node.dart
@@ -1,11 +1,24 @@
 part of tree.collection;
 
-class Node<K, V> {
+class Node<K, V> extends Equatable {
   Node({required this.key, this.data}) : children = [];
 
   final K key;
-  V? data;
+  final V? data;
   final List<Node<K, V>> children;
 
   bool get hasChildren => children.isNotEmpty;
+
+  Node<K, V> copyWith({K? key, V? data, List<Node<K, V>>? children}) {
+    final node = Node(key: key ?? this.key, data: data ?? this.data);
+    if (children != null) {
+      node.children.addAll(children);
+    } else {
+      node.children.addAll(this.children);
+    }
+    return node;
+  }
+
+  @override
+  List<Object?> get props => [key, data, children];
 }
--- lib/tree_collection.dart
+++ lib/tree_collection.dart
@@ -2,6 +2,8 @@ library tree.collection;
 
 import 'dart:collection';
 
+import 'package:equatable/equatable.dart';
+
 part 'src/node.dart';
 part 'src/traversal.dart';
 part 'src/tree.dart';
